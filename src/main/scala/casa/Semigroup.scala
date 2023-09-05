d4a05123f5e7aababa50a782926ba62d79acef6d
jcaraballo <jcaraballo@users.noreply.github.com>
2023-09-05 23:27:18 +0100

    conciser

--- src/main/scala/casa/Semigroup.scala
+++ src/main/scala/casa/Semigroup.scala
@@ -5,4 +5,4 @@ trait Semigroup[A]:
 
 object Semigroup:
   object Instances:
-    given nonEmptyListConcatenationSemigroup[A]: Semigroup[Nel[A]] = (a1: Nel[A], a2: Nel[A]) => a1 ::: a2
+    given nonEmptyListConcatenationSemigroup[A]: Semigroup[Nel[A]] = _ ::: _
